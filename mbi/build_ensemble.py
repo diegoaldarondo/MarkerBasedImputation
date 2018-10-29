0829d5ea29bccec3787afb8cc1864f8a5d4f1ea5
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2018-10-29 11:37:38 -0400

    Finished linting

--- mbi/build_ensemble.py
+++ mbi/build_ensemble.py
@@ -1,98 +1,96 @@
-import numpy as np
-import h5py
-import os
-from time import time
-import re
-import shutil
-import inspect
-import datetime
-from subprocess import check_call
-from skimage import measure
-from scipy.io import loadmat, savemat
-from scipy import stats
-import matplotlib.pyplot as plt
+"""Build a model ensemble."""
 import clize
-import keras
+from keras.models import Model, load_model
+from keras.layers import Input, Lambda, concatenate
+import numpy as np
+import os
+from scipy.io import savemat
 import tensorflow as tf
-from utils import CausalAtrousConvolution1D
-from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, LambdaCallback
-from keras import backend as K
-from keras.models import Model, load_model, save_model
-from keras.layers import Input, Conv1D, Dense, Dropout, Lambda, Permute, LSTM, Average, concatenate, Layer, multiply
-from keras.optimizers import Adam
 from utils import create_run_folders
 
-## Make an ensemble
+
 def ensemble(models, model_input, return_member_data):
-    """
-    Build an ensemble of models that output the median of all members. Does not compile the ensemble.
-    :param models: List of keras models to include in the ensemble. Currently requires the same output shape.
-    :param model_input: Input shape of the members. Used for building the ensemble.
-    :param return_member_data: If True, model will have two outputs: the ensemble prediction and all member predictions.
-                               Otherwise, the model will output only the ensemble predictions.
+    """Build an ensemble of models that output the median of all members.
+
+    Note: Does not compile the ensemble.
+    :param models: List of keras models to include in the ensemble. Currently
+                   requires the same output shape.
+    :param model_input: Input shape of the members. Used for building the
+                        ensemble.
+    :param return_member_data: If True, model will have two outputs: the
+                               ensemble prediction and all member predictions.
+                               Otherwise, the model will output only the
+                               ensemble predictions.
     """
     def ens_median(x):
-        import tensorflow as tf # This is needed to load the model in the future.
-        return tf.contrib.distributions.percentile(x,50,axis=1)
-    def pad(x):
-        return(x[:,None,:])
+        import tensorflow as tf # This is needed to load the model.
+        return tf.contrib.distributions.percentile(x, 50, axis=1)
 
-    # Get outputs from the ensemble models, compute the median, and fix the shape.
+    def pad(x):
+        return(x[:, None, :])
+
+    # Get outputs from the ensemble models, compute the median, and fix the
+    # shape.
     outputs = [model(model_input) for model in models]
-    member_predictions = concatenate(outputs,axis = 1)
+    member_predictions = concatenate(outputs, axis=1)
     ensemble_prediction = Lambda(ens_median)(member_predictions)
     ensemble_prediction = Lambda(pad)(ensemble_prediction)
 
-    # Return model. No compilation is necessary since there are no additional trainable parameters.
+    # Return model. No compilation is necessary since there are no additional
+    # trainable parameters.
     if return_member_data:
-        model = Model(model_input, outputs=[ensemble_prediction,member_predictions], name='ensemble')
+        model = Model(model_input,
+                      outputs=[ensemble_prediction, member_predictions],
+                      name='ensemble')
     else:
         model = Model(model_input, ensemble_prediction, name='ensemble')
     return model
 
-def build_ensemble(base_output_path,
-    *models_in_ensemble,
-    return_member_data = True,
-    run_name=None,
-    clean=False):
-    """
-    Build an ensemble of models for marker prediction
+
+def build_ensemble(base_output_path, *models_in_ensemble,
+                   return_member_data=True, run_name=None, clean=False):
+    """Build an ensemble of models for marker prediction.
+
     :param base_output_path: Path to base models directory
-    :param models_in_ensemble: List of all of the models to be included in the build_ensemble
-    :param return_member_data: If True, model will have two outputs: the ensemble prediction and all member predictions.
+    :param models_in_ensemble: List of all of the models to be included in the
+                               build_ensemble
+    :param return_member_data: If True, model will have two outputs: the
+                               ensemble prediction and all member predictions.
     :param run_name: Name of the model run
     :param clean: If True, deletes the contents of the run output path
     """
-
     # Load all of the models to be used as members of the ensemble
     models = [None]*len(models_in_ensemble)
     for i in range(len(models_in_ensemble)):
-        models[i] = load_model(os.path.join(base_output_path, models_in_ensemble[i]))
+        models[i] = load_model(os.path.join(base_output_path,
+                                            models_in_ensemble[i]))
         models[i].name = 'model_%d' % (i)
 
     # Build the ensemble
     ensemble_input = Input(batch_shape=models[0].input_shape)
-    model_ensemble = ensemble(models,ensemble_input,return_member_data)
+    model_ensemble = ensemble(models, ensemble_input, return_member_data)
 
     # Build ensemble folder name if needed
-    if run_name == None:
+    if run_name is None:
         run_name = "model_ensemble"
     print("run_name:", run_name)
 
     # Initialize run directories
     print('Building run folders')
-    run_path = create_run_folders(run_name, base_path=base_output_path, clean=clean)
+    run_path = create_run_folders(run_name, base_path=base_output_path,
+                                  clean=clean)
 
     # Convert list of models to objects for .mat saving
-    model_paths = np.empty((len(models_in_ensemble),), dtype=np.object)
+    model_paths = np.empty((len(models_in_ensemble), ), dtype=np.object)
     for i in range(len(models_in_ensemble)):
         model_paths[i] = models_in_ensemble[i]
 
     # Save the training information in a mat file.
     print('Saving training info')
     savemat(os.path.join(run_path, "training_info.mat"),
-            {"base_output_path": base_output_path, "run_name": run_name, "return_member_data": return_member_data,
-             "clean": clean,"model_paths":model_paths,"n_members":len(models_in_ensemble)})
+            {"base_output_path": base_output_path, "run_name": run_name,
+             "return_member_data": return_member_data, "clean": clean,
+             "model_paths": model_paths, "n_members": len(models_in_ensemble)})
 
     print('Saving model ensemble')
     model_ensemble.save(os.path.join(run_path, "final_model.h5"))
--- mbi/utils.py
+++ mbi/utils.py
@@ -1,37 +1,24 @@
-import numpy as np
+"""Utitlity functions for mbi."""
 import h5py
-import os
-from time import time
-from scipy.io import loadmat, savemat
-import re
-import shutil
-import inspect
-import datetime
-from subprocess import check_call
-
-import keras
-import keras.losses
-from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, LambdaCallback
 from keras import backend as K
-from keras.models import Model, load_model
-from keras.layers import Input, Conv1D, Dense, Dropout, Lambda, Permute, LSTM
-from keras.optimizers import Adam
-from keras.utils import multi_gpu_model
-from keras.layers.convolutional import Conv1D
+from keras.layers import Conv1D
 from keras.utils.conv_utils import conv_output_length
+import numpy as np
+import os
+import shutil
 import tensorflow as tf
 
+
 def load_dataset(data_path):
-    """
-    Load marker, marker_means, marker_stds, and bad_frames from .h5 dataset
+    """Load marker, marker_means, marker_stds, and bad_frames from .h5 dataset.
 
     :param data_path: Path to .h5 file
-
     Outputs:
     markers - Z-scored marker trajectories over time.
     marker_means - Mean of markers in real world coordinates
     marker_stds - Std of markers in real world coordinates
-    bad_frames - Logical or integer matrix denoting frames in which there were marker errors. Same size as markers.
+    bad_frames - Logical or integer matrix denoting frames in which there were
+                 marker errors. Same size as markers.
                  0 when marker j is a good recording at frame i, 1 otherwise.
     """
     f = h5py.File(data_path, 'r')
@@ -42,64 +29,69 @@ def load_dataset(data_path):
 
     return markers, marker_means, marker_stds, bad_frames
 
-def get_ids(bad_frames, input_length, output_length, only_good_inputs = False, only_good_outputs = False):
-    """
-    Get the sequences of indices to use for network training.
+
+def get_ids(bad_frames, input_length, output_length, only_good_inputs=False,
+            only_good_outputs=False):
+    """Get the sequences of indices to use for network training.
 
     Inputs:
-    bad_frames - Logical or integer matrix denoting frames in which there were marker errors. Same size as markers.
-                 0 when marker j is a good recording at frame i, 1 otherwise.
+    bad_frames - Logical or integer matrix denoting frames in which there were
+                 marker errors. Same size as markers. 0 when marker j is a good
+                 recording at frame i, 1 otherwise.
     input_length - Number of frames to input to the model.
     output_length - Number of frames the model will attempt to predict.
 
     Optional:
-    only_good_inputs - Only return samples in which there are no bad frames in the input sequence. Default: False
-    only_good_outputs - Only return samples in which there are no bad frames in the output sequence. Default: False
+    only_good_inputs - Only return samples in which there are no bad frames in
+                       the input sequence. Default: False
+    only_good_outputs - Only return samples in which there are no bad frames in
+                        the output sequence. Default: False
 
     Outputs:
     input_ids - N x input_length integer matrix of input ids.
     output_ids - N x output_length integer matrix of output ids.
-
     """
     # Find all of the good frames
-    n_bad_markers = np.sum(bad_frames,1)
+    n_bad_markers = np.sum(bad_frames, 1)
     good_frames = np.where(n_bad_markers == 0)[0]
-    good_frames = good_frames[(good_frames > input_length) & (good_frames < (bad_frames.shape[0]-output_length))]
+    good_frames = \
+        good_frames[(good_frames > input_length) &
+                    (good_frames < (bad_frames.shape[0]-output_length))]
 
     # Save the preceding input_length ids before that frame
-    input_ids = np.zeros([good_frames.shape[0],input_length],'int32')
-    output_ids = np.zeros([good_frames.shape[0],output_length],'int32')
+    input_ids = np.zeros([good_frames.shape[0], input_length], 'int32')
+    output_ids = np.zeros([good_frames.shape[0], output_length], 'int32')
 
     for i in range(input_ids.shape[0]):
-        input_ids[i,:] = range(good_frames[i]-input_length,good_frames[i])
-        output_ids[i,:] = range(good_frames[i],good_frames[i]+output_length)
+        input_ids[i, :] = range(good_frames[i]-input_length, good_frames[i])
+        output_ids[i, :] = range(good_frames[i], good_frames[i]+output_length)
 
     # Remove all samples that have bad frames in the input
     if only_good_inputs:
-        n_bad_markers_all = n_bad_markers[input_ids];
+        n_bad_markers_all = n_bad_markers[input_ids]
         is_bad_input = n_bad_markers_all != 0
-        is_bad_sample = np.any(is_bad_input,axis=1)
-        input_ids = input_ids[~is_bad_sample,:]
-        output_ids = output_ids[~is_bad_sample,:]
+        is_bad_sample = np.any(is_bad_input, axis=1)
+        input_ids = input_ids[~is_bad_sample, :]
+        output_ids = output_ids[~is_bad_sample, :]
 
     # Remove all samples that have bad frames in the output
     if only_good_outputs:
-        n_bad_markers_all = n_bad_markers[output_ids];
+        n_bad_markers_all = n_bad_markers[output_ids]
         is_bad_output = n_bad_markers_all != 0
-        is_bad_sample = np.any(is_bad_output,axis=1)
-        input_ids = input_ids[~is_bad_sample,:]
-        output_ids = output_ids[~is_bad_sample,:]
+        is_bad_sample = np.any(is_bad_output, axis=1)
+        input_ids = input_ids[~is_bad_sample, :]
+        output_ids = output_ids[~is_bad_sample, :]
 
     return input_ids, output_ids
 
+
 def create_run_folders(run_name, base_path="models", clean=False):
-    """
-    Creates subfolders necessary for outputs of training.
+    """Create subfolders necessary for outputs of training.
+
     :param run_name: Name of the model folder
     :param base_path: Base path in which to store models
     :param clean: If True, deletes the contents of the run output path
     """
-
     def is_empty_run(run_path):
         weights_path = os.path.join(run_path, "weights")
         has_weights_folder = os.path.exists(weights_path)
@@ -110,7 +102,7 @@ def create_run_folders(run_name, base_path="models", clean=False):
     if not clean:
         initial_run_path = run_path
         i = 1
-        while os.path.exists(run_path): #and not is_empty_run(run_path):
+        while os.path.exists(run_path):
             run_path = "%s_%02d" % (initial_run_path, i)
             i += 1
 
@@ -124,10 +116,12 @@ def create_run_folders(run_name, base_path="models", clean=False):
 
     return run_path
 
+
 def asymmetric_temporal_padding(x, left_pad=1, right_pad=1):
-    '''Pad the middle dimension of a 3D tensor
+    """Pad the middle dimension of a 3D tensor.
+
     with "left_pad" zeros left and "right_pad" right.
-    '''
+    """
     pattern = [[0, 0], [left_pad, right_pad], [0, 0]]
     return tf.pad(x, pattern)
 
@@ -139,12 +133,16 @@ def categorical_mean_squared_error(y_true, y_pred):
 
 
 class CausalAtrousConvolution1D(Conv1D):
-    def __init__(self, filters, kernel_size, init='glorot_uniform', activation=None,
-                 padding='valid', strides=1, dilation_rate=1, bias_regularizer=None,
-                 activity_regularizer=None, kernel_constraint=None, bias_constraint=None, use_bias=True, causal=False, **kwargs):
+    """AtrousConvolution for use in res_skips: currently not implemented."""
+
+    def __init__(self, filters, kernel_size, init='glorot_uniform',
+                 activation=None, padding='valid', strides=1, dilation_rate=1,
+                 bias_regularizer=None, activity_regularizer=None,
+                 kernel_constraint=None, bias_constraint=None, use_bias=True,
+                 causal=False, **kwargs):
         super(CausalAtrousConvolution1D, self).__init__(filters,
                                                         kernel_size=kernel_size,
-                                                        strides=strides,
+                                                        strides=strides
                                                         padding=padding,
                                                         dilation_rate=dilation_rate,
                                                         activation=activation,
@@ -176,5 +174,6 @@ class CausalAtrousConvolution1D(Conv1D):
 
     def call(self, x):
         if self.causal:
-            x = asymmetric_temporal_padding(x, self.dilation_rate[0] * (self.kernel_size[0] - 1), 0)
+            x = asymmetric_temporal_padding(x, self.dilation_rate[0] *
+                                            (self.kernel_size[0] - 1), 0)
         return super(CausalAtrousConvolution1D, self).call(x)
